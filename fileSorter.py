dd6619a139171534c076c267df7fa2f69cc7a5fb
poopachu9000 <71670016+poopachu9000@users.noreply.github.com>
2022-02-13 17:25:16 -0600

    Add files via upload

---
+++ fileSorter.py
@@ -0,0 +1,13 @@
+def swapFileData():
+    data1 =input('What is the first file? ')
+    print(data1)
+    data2 =input('What is the second file? ')
+    # with open(data1, 'r') as a:
+    #     data_a = a.read()
+    # with open(data2, 'r') as b:
+    #     data_b = b.read()
+    # with open(data1, 'w') as a:
+    #     a.write(data_b)
+    # with open(data2, 'w') as b:
+    #     b.write(data_a)
+swapFileData()
